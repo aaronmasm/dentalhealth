93d983878ef19b86a38b57497ef158cac1aa61e6
aaronmasm <aaronmasm85@gmail.com>
2024-07-31 09:02:00 -0500

    A footer and a whatsapp button were added to the base.html. Also modified the navbar structure to make it look good. base.html and home.html page are finished.

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('#whatsapp').floatingWhatsApp({
+        phone: '51933488855',
+        position: 'right',
+        zIndex: 1,
+        popupMessage: 'Hi! One of our team members is ready to chat via WhatsApp.',
+        message: "I'd like to make an inquiry.",
+        showPopup: true,
+        showOnIE: false,
+        headerTitle: 'DentalHealth',
+    });
+});
